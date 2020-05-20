c4efcf96ffca830b6e5d94cbf4563352b1c125ee
Shiv Kushwah <shivendrakushwah@gmail.com>
2020-05-20 14:36:26 -0700

    cleaned up network_simulator.cpp

--- Samples/PSecureV1/Src/app/network_simulator.cpp
+++ Samples/PSecureV1/Src/app/network_simulator.cpp
@@ -16,8 +16,14 @@ extern int host_machine_port;
 extern int KPS_PORT_ATTESTATION;
 extern int KPS_PORT_GENERIC;
 
-// Function designed for chat between client and server. 
-void func(int sockfd) 
+const int DISTRIBUTED_HOST_READ_REQUEST = 1;
+const int KPS_ATTESTATION_READ_REQUEST = 2;
+const int KPS_GENERIC_READ_REQUEST = 3;
+
+// Reading Incoming Network Request Handlers *******************
+
+// Function that reads incoming data from the network and forwards it to send_network_request_API and returns the results back over the network
+void func(int sockfd, int handle_incoming_request_type) 
 { 
 	char buff[MAX]; 
 	int n; 
@@ -32,57 +38,21 @@ void func(int sockfd)
 			ocall_print("NOTE: Server Network buffer full\n");
 		}
 
-		char* retString = send_network_request_API(buff, actual_response_size);
-		// char* retString = "netreceive";
-
-
-		// print buffer which contains the client contents 
-		// ocall_print("From client: %s\t To client : ", buff); 
-		bzero(buff, MAX); 
-		n = 0; 
-		// copy server message in the buffer 
-		ocall_print("Server: copying into return buffer\n");
-		printPayload(retString, 20);
-		ocall_print("Server: about to memcpy\n");
-		memcpy(buff, retString, 1000);  //TODO unhardcode 1000 and have make network request
-										//return values have size assoicated with them 
-		
-
-		// and send that buffer to client 
-		write(sockfd, buff, sizeof(buff)); 
-
-		// if msg contains "Exit" then server exit and chat ended. 
-		// if (strncmp("exit", buff, 4) == 0) { 
-		// 	ocall_print("Server Exit...\n"); 
-		// 	break; 
-		// } 
-		ocall_print("Server sent message to client!\n");
-        break;
-	} 
-} 
-
-// Function designed for chat between client and server. 
-void func_attestation(int sockfd) 
-{ 
-	char buff[MAX]; 
-	int n; 
-	// infinite loop for chat 
-	for (;;) { 
-		bzero(buff, MAX); 
-
-		// read the message from client and copy it in buffer 
-		int actual_response_size = read(sockfd, buff, sizeof(buff)); 
-
-		if (actual_response_size >= sizeof(buff)) {
-			ocall_print("NOTE: Server Network buffer full\n");
-		}
-		
 		int responseSize;
-		char* retString = handle_socket_attestation_request(buff, responseSize);
-		// char* retString = "netreceive";
-
-		if (responseSize >= MAX) {
-			ocall_print("ERROR: Server network buffer overflow\n");
+		char* retString;
+		if (handle_incoming_request_type == DISTRIBUTED_HOST_READ_REQUEST) {
+			responseSize = 1000; //TODO unhardcode 1000 and have make network request return values that have size associated with them 
+			retString = send_network_request_API(buff, actual_response_size);
+		} else if (handle_incoming_request_type == KPS_ATTESTATION_READ_REQUEST) {
+			retString = handle_socket_attestation_request(buff, responseSize);
+			if (responseSize >= MAX) {
+				ocall_print("ERROR: Server network buffer overflow\n");
+			}
+		} else {
+			retString = handle_socket_kps_generic_request(buff, responseSize);
+			if (responseSize >= MAX) {
+				ocall_print("ERROR: Server network buffer overflow\n");
+			}
 		}
 
 
@@ -94,58 +64,7 @@ void func_attestation(int sockfd)
 		ocall_print("Server: copying into return buffer\n");
 		printPayload(retString, 20);
 		ocall_print("Server: about to memcpy\n");
-		memcpy(buff, retString, responseSize);  //TODO unhardcode 1000 and have make network request
-										//return values have size assoicated with them 
-		
-
-		// and send that buffer to client 
-		write(sockfd, buff, responseSize); 
-
-		// if msg contains "Exit" then server exit and chat ended. 
-		// if (strncmp("exit", buff, 4) == 0) { 
-		// 	ocall_print("Server Exit...\n"); 
-		// 	break; 
-		// } 
-		ocall_print("Server sent message to client!\n");
-        break;
-	} 
-} 
-
-// Function designed for chat between client and server. 
-void func_kps_generic(int sockfd) 
-{ 
-	char buff[MAX]; 
-	int n; 
-	// infinite loop for chat 
-	for (;;) { 
-		bzero(buff, MAX); 
-
-		// read the message from client and copy it in buffer 
-		int actual_response_size = read(sockfd, buff, sizeof(buff)); 
-
-		if (actual_response_size >= sizeof(buff)) {
-			ocall_print("NOTE: Server Network buffer full\n");
-		}
-		
-		int responseSize;
-		char* retString = handle_socket_kps_generic_request(buff, responseSize);
-		// char* retString = "netreceive";
-
-		if (responseSize >= MAX) {
-			ocall_print("ERROR: Server network buffer overflow\n");
-		}
-
-
-		// print buffer which contains the client contents 
-		// ocall_print("From client: %s\t To client : ", buff); 
-		bzero(buff, MAX); 
-		n = 0; 
-		// copy server message in the buffer 
-		ocall_print("Server: copying into return buffer\n");
-		printPayload(retString, 20);
-		ocall_print("Server: about to memcpy\n");
-		memcpy(buff, retString, responseSize);  //TODO unhardcode 1000 and have make network request
-										//return values have size assoicated with them 
+		memcpy(buff, retString, responseSize);  
 		
 
 		// and send that buffer to client 
@@ -163,326 +82,164 @@ void func_kps_generic(int sockfd)
 
 void* server_handle_connection_thread(void* arg) {
 	int sockfd = *((int *)arg);
-	func(sockfd);
+	func(sockfd, DISTRIBUTED_HOST_READ_REQUEST);
 	close(sockfd);
 
 }
 
 void* server_handle_attestation_connection_thread(void* arg) {
 	int sockfd = *((int *)arg);
-	func_attestation(sockfd);
+	func(sockfd, KPS_ATTESTATION_READ_REQUEST);
 	close(sockfd);
 
 }
 
 void* server_handle_kps_generic_connection_thread(void* arg) {
 	int sockfd = *((int *)arg);
-	func_kps_generic(sockfd);
+	func(sockfd, KPS_GENERIC_READ_REQUEST);
 	close(sockfd);
 
 }
 
-// Driver function 
+void handle_socket_helper(void* arg, int handle_incoming_request_type) {
+	int sockfd, connfd, len; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		ocall_print("Server Socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		ocall_print("Server Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	int enable = 1;
+
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+		ocall_print("server setsockopt(SO_REUSEADDR) failed\n");
+	}
+    
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+
+	if (handle_incoming_request_type == DISTRIBUTED_HOST_READ_REQUEST) {
+		servaddr.sin_port = htons(host_machine_port);
+	} else if (handle_incoming_request_type == KPS_ATTESTATION_READ_REQUEST) {
+		servaddr.sin_port = htons(KPS_PORT_ATTESTATION); 
+	} else {
+		servaddr.sin_port = htons(KPS_PORT_GENERIC);
+	}
+
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+		if (errno == EADDRINUSE) {
+			ocall_print("Error: Address already in use!\n");
+		}
+		ocall_print("Server Socket bind failed...\n"); 
+		exit(0); 
+	} 
+	else
+		ocall_print("Server Socket successfully binded..\n"); 
+
+	// -------------------------------------------------------
+	
+	// int sockfd2, connfd2, len2; 
+	// struct sockaddr_in servaddr2, cli2; 
+
+	// // socket create and verification 
+	// sockfd2 = socket(AF_INET, SOCK_STREAM, 0); 
+	// if (sockfd2 == -1) { 
+	// 	ocall_print("socket2 creation failed...\n"); 
+	// 	exit(0); 
+	// } 
+	// else
+	// 	ocall_print("Socket2 successfully created..\n"); 
+	// bzero(&servaddr2, sizeof(servaddr2)); 
+
+	// // assign IP, PORT 
+	// servaddr2.sin_family = AF_INET; 
+	// servaddr2.sin_addr.s_addr = htonl(INADDR_ANY); 
+	// servaddr2.sin_port = htons(PORT2); 
+
+	// // Binding newly created socket to given IP and verification 
+	// if ((bind(sockfd2, (SA*)&servaddr2, sizeof(servaddr2))) != 0) { 
+	// 	ocall_print("socket2 bind failed...\n"); 
+	// 	exit(0); 
+	// } 
+	// else
+	// 	ocall_print("Socket2 successfully binded..\n");
+	
+	// -----------------------------
+
+	// Now server is ready to listen and verification 
+	if ((listen(sockfd, 5)) != 0) { 
+		if (errno == EADDRINUSE) {
+			ocall_print("ERROR: Server Address already in use!\n");
+		} else if (errno == EBADF) {
+			ocall_print("ERROR: Server sockfd is not valid for listen command!\n");
+
+		}
+		ocall_print("Server listen failed...\n"); 
+		exit(0); 
+	} 
+	else
+		ocall_print("Server listening..\n"); 
+	len = sizeof(cli);
+
+    while (1) {
+        // Accept the data packet from client and verification 
+        connfd = accept(sockfd, (SA*)&cli, (socklen_t*)&len); 
+        if (connfd < 0) { 
+            ocall_print("server acccept failed...\n"); 
+            exit(0); 
+        } 
+        else
+            ocall_print("server acccept the client...\n"); 
+
+		pthread_t pid;
+		if (handle_incoming_request_type == DISTRIBUTED_HOST_READ_REQUEST) {
+			pthread_create(&pid, NULL, server_handle_connection_thread, &connfd);
+		} else if (handle_incoming_request_type == KPS_ATTESTATION_READ_REQUEST) {
+			pthread_create(&pid, NULL, server_handle_attestation_connection_thread, &connfd); 
+		} else {
+			pthread_create(&pid, NULL, server_handle_kps_generic_connection_thread, &connfd);
+		}
+
+        // After chatting close the socket 
+        // close(connfd); 
+    }
+}
+
 void* handle_socket_network_request(void* arg) 
 { 
-	int sockfd, connfd, len; 
-	struct sockaddr_in servaddr, cli; 
-
-	// socket create and verification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		ocall_print("Server Socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
-
-	int enable = 1;
-
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
-		ocall_print("server setsockopt(SO_REUSEADDR) failed\n");
-	}
-    
-
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	int port = host_machine_port;
-
-	servaddr.sin_port = htons(port); 
-
-	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-		if (errno == EADDRINUSE) {
-			ocall_print("Error: Address already in use!\n");
-		}
-		ocall_print("Server Socket bind failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server Socket successfully binded..\n"); 
-
-	// -------------------------------------------------------
-	
-	// int sockfd2, connfd2, len2; 
-	// struct sockaddr_in servaddr2, cli2; 
-
-	// // socket create and verification 
-	// sockfd2 = socket(AF_INET, SOCK_STREAM, 0); 
-	// if (sockfd2 == -1) { 
-	// 	ocall_print("socket2 creation failed...\n"); 
-	// 	exit(0); 
-	// } 
-	// else
-	// 	ocall_print("Socket2 successfully created..\n"); 
-	// bzero(&servaddr2, sizeof(servaddr2)); 
-
-	// // assign IP, PORT 
-	// servaddr2.sin_family = AF_INET; 
-	// servaddr2.sin_addr.s_addr = htonl(INADDR_ANY); 
-	// servaddr2.sin_port = htons(PORT2); 
-
-	// // Binding newly created socket to given IP and verification 
-	// if ((bind(sockfd2, (SA*)&servaddr2, sizeof(servaddr2))) != 0) { 
-	// 	ocall_print("socket2 bind failed...\n"); 
-	// 	exit(0); 
-	// } 
-	// else
-	// 	ocall_print("Socket2 successfully binded..\n");
-	
-	// -----------------------------
-
-	// Now server is ready to listen and verification 
-	if ((listen(sockfd, 5)) != 0) { 
-		if (errno == EADDRINUSE) {
-			ocall_print("ERROR: Server Address already in use!\n");
-		} else if (errno == EBADF) {
-			ocall_print("ERROR: Server sockfd is not valid for listen command!\n");
-
-		}
-		ocall_print("Server listen failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server listening..\n"); 
-	len = sizeof(cli);
-
-    while (1) {
-        // Accept the data packet from client and verification 
-        connfd = accept(sockfd, (SA*)&cli, (socklen_t*)&len); 
-        if (connfd < 0) { 
-            ocall_print("server acccept failed...\n"); 
-            exit(0); 
-        } 
-        else
-            ocall_print("server acccept the client...\n"); 
-
-		pthread_t pid;
-		pthread_create(&pid, NULL, server_handle_connection_thread, &connfd);
-
-        // Function for chatting between client and server 
-        // func(connfd); 
-
-        // After chatting close the socket 
-        // close(connfd); 
-    }
-	
+	handle_socket_helper(arg, DISTRIBUTED_HOST_READ_REQUEST);
 } 
-// Driver function 
 void* handle_socket_network_request_attestation(void* arg) 
 { 
-	int sockfd, connfd, len; 
-	struct sockaddr_in servaddr, cli; 
-
-	// socket create and verification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		ocall_print("Server Socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
-
-	int enable = 1;
-
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
-		ocall_print("server setsockopt(SO_REUSEADDR) failed\n");
-	}
-    
-
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	servaddr.sin_port = htons(KPS_PORT_ATTESTATION); 
-
-	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-		if (errno == EADDRINUSE) {
-			ocall_print("Error: Address already in use!\n");
-		}
-		ocall_print("Server Socket bind failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server Socket successfully binded..\n"); 
-
-	// -------------------------------------------------------
-	
-	// int sockfd2, connfd2, len2; 
-	// struct sockaddr_in servaddr2, cli2; 
-
-	// // socket create and verification 
-	// sockfd2 = socket(AF_INET, SOCK_STREAM, 0); 
-	// if (sockfd2 == -1) { 
-	// 	ocall_print("socket2 creation failed...\n"); 
-	// 	exit(0); 
-	// } 
-	// else
-	// 	ocall_print("Socket2 successfully created..\n"); 
-	// bzero(&servaddr2, sizeof(servaddr2)); 
-
-	// // assign IP, PORT 
-	// servaddr2.sin_family = AF_INET; 
-	// servaddr2.sin_addr.s_addr = htonl(INADDR_ANY); 
-	// servaddr2.sin_port = htons(PORT2); 
-
-	// // Binding newly created socket to given IP and verification 
-	// if ((bind(sockfd2, (SA*)&servaddr2, sizeof(servaddr2))) != 0) { 
-	// 	ocall_print("socket2 bind failed...\n"); 
-	// 	exit(0); 
-	// } 
-	// else
-	// 	ocall_print("Socket2 successfully binded..\n");
-	
-	// -----------------------------
-
-	// Now server is ready to listen and verification 
-	if ((listen(sockfd, 5)) != 0) { 
-		if (errno == EADDRINUSE) {
-			ocall_print("ERROR: Server Address already in use!\n");
-		} else if (errno == EBADF) {
-			ocall_print("ERROR: Server sockfd is not valid for listen command!\n");
-
-		}
-		ocall_print("Server listen failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server listening..\n"); 
-	len = sizeof(cli);
-
-    while (1) {
-        // Accept the data packet from client and verification 
-        connfd = accept(sockfd, (SA*)&cli, (socklen_t*)&len); 
-        if (connfd < 0) { 
-            ocall_print("server acccept failed...\n"); 
-            exit(0); 
-        } 
-        else
-            ocall_print("server acccept the client...\n"); 
-
-		pthread_t pid;
-		pthread_create(&pid, NULL, server_handle_attestation_connection_thread, &connfd);
-
-        // Function for chatting between client and server 
-        // func(connfd); 
-
-        // After chatting close the socket 
-        // close(connfd); 
-    }
-	
+	handle_socket_helper(arg, KPS_ATTESTATION_READ_REQUEST);
 } 
 
-// Driver function 
 void* handle_socket_network_kps_generic_requests(void* arg) 
 { 
-	int sockfd, connfd, len; 
-	struct sockaddr_in servaddr, cli; 
-
-	// socket create and verification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		ocall_print("Server Socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
-
-	int enable = 1;
-
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
-		ocall_print("server setsockopt(SO_REUSEADDR) failed\n");
-	}
-    
-
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	servaddr.sin_port = htons(KPS_PORT_GENERIC); 
-
-	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-		if (errno == EADDRINUSE) {
-			ocall_print("Error: Address already in use!\n");
-		}
-		ocall_print("Server Socket bind failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server Socket successfully binded..\n"); 
-
-	// Now server is ready to listen and verification 
-	if ((listen(sockfd, 5)) != 0) { 
-		if (errno == EADDRINUSE) {
-			ocall_print("ERROR: Server Address already in use!\n");
-		} else if (errno == EBADF) {
-			ocall_print("ERROR: Server sockfd is not valid for listen command!\n");
-
-		}
-		ocall_print("Server listen failed...\n"); 
-		exit(0); 
-	} 
-	else
-		ocall_print("Server listening..\n"); 
-	len = sizeof(cli);
-
-    while (1) {
-        // Accept the data packet from client and verification 
-        connfd = accept(sockfd, (SA*)&cli, (socklen_t*)&len); 
-        if (connfd < 0) { 
-            ocall_print("server acccept failed...\n"); 
-            exit(0); 
-        } 
-        else
-            ocall_print("server acccept the client...\n"); 
-
-		pthread_t pid;
-		pthread_create(&pid, NULL, server_handle_kps_generic_connection_thread, &connfd);
-
-        // Function for chatting between client and server 
-        // func(connfd); 
-
-        // After chatting close the socket 
-        // close(connfd); 
-    }
-	
+	handle_socket_helper(arg, KPS_GENERIC_READ_REQUEST);
 } 
 
+//*******************
 
+
+// Sending Outgoing Network Request Methods *******************
+
+// Function that sends outgoing data across the network to appropiate machine and returns response
 void func_sender(int sockfd, char* request, int request_size, char* network_response) 
 { 
 	char buff[MAX]; 
-	int n; 
 	for (;;) { 
 		bzero(buff, sizeof(buff)); 
 		memcpy(buff, request, request_size);
-		// ocall_print("Enter the string : "); 
-		n = 0; 
-		// while ((buff[n++] = getchar()) != '\n') 
-		// 	; 
 		write(sockfd, buff, request_size); 
 		bzero(buff, sizeof(buff)); 
 		read(sockfd, buff, sizeof(buff)); 
@@ -563,3 +320,4 @@ char* network_socket_sender(char* request, int request_size, char* ipAddress, in
 
 	return network_response;
 } 
+//*******************
--- docs/ArchitectureOverview.md
+++ docs/ArchitectureOverview.md
@@ -5,6 +5,8 @@
 
 The distributed host machine runs the `main` method in app.cpp first. This spawns a thread to listen for network requests from other distributed host machines. This also spawns the appropiate P Process to host USMs in this host and parses the command line instructions in order to create the first USM in the system or wait to receive a network message from the start distributed host. App.cpp contains all of the USM hosting code and various foreign functions to handle USM operations (such as USM creation, USM message sending). 
 
+Network_simulator.cpp contains all of the functions for handling network socket requests and sending out network socket requests to other distributed host machines. Incoming requests are forwarded to the `send_network_request_API` in network_ra.cpp.
+
 
 ### KPS
 
