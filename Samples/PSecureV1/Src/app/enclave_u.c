75b7fc261a0f9c599a774ccdc7ccf2eedd4cfd42
Shiv Kushwah <shivendrakushwah@gmail.com>
2020-10-31 18:03:45 +0000

    updated generated enclave files to be generated by latest sgx sdk, this built successfully in hw docker

--- Samples/PSecureV1/Src/app/enclave_u.c
+++ Samples/PSecureV1/Src/app/enclave_u.c
@@ -361,39 +361,6 @@ typedef struct ms_sgx_thread_set_multiple_untrusted_events_ocall_t {
 	size_t ms_total;
 } ms_sgx_thread_set_multiple_untrusted_events_ocall_t;
 
-typedef struct ms_create_session_ocall_t {
-	sgx_status_t ms_retval;
-	uint32_t* ms_sid;
-	uint8_t* ms_dh_msg1;
-	uint32_t ms_dh_msg1_size;
-	uint32_t ms_timeout;
-} ms_create_session_ocall_t;
-
-typedef struct ms_exchange_report_ocall_t {
-	sgx_status_t ms_retval;
-	uint32_t ms_sid;
-	uint8_t* ms_dh_msg2;
-	uint32_t ms_dh_msg2_size;
-	uint8_t* ms_dh_msg3;
-	uint32_t ms_dh_msg3_size;
-	uint32_t ms_timeout;
-} ms_exchange_report_ocall_t;
-
-typedef struct ms_close_session_ocall_t {
-	sgx_status_t ms_retval;
-	uint32_t ms_sid;
-	uint32_t ms_timeout;
-} ms_close_session_ocall_t;
-
-typedef struct ms_invoke_service_ocall_t {
-	sgx_status_t ms_retval;
-	uint8_t* ms_pse_message_req;
-	uint32_t ms_pse_message_req_size;
-	uint8_t* ms_pse_message_resp;
-	uint32_t ms_pse_message_resp_size;
-	uint32_t ms_timeout;
-} ms_invoke_service_ocall_t;
-
 static sgx_status_t SGX_CDECL enclave_ocall_print(void* pms)
 {
 	ms_ocall_print_t* ms = SGX_CAST(ms_ocall_print_t*, pms);
@@ -544,43 +511,11 @@ static sgx_status_t SGX_CDECL enclave_sgx_thread_set_multiple_untrusted_events_o
 	return SGX_SUCCESS;
 }
 
-static sgx_status_t SGX_CDECL enclave_create_session_ocall(void* pms)
-{
-	ms_create_session_ocall_t* ms = SGX_CAST(ms_create_session_ocall_t*, pms);
-	ms->ms_retval = create_session_ocall(ms->ms_sid, ms->ms_dh_msg1, ms->ms_dh_msg1_size, ms->ms_timeout);
-
-	return SGX_SUCCESS;
-}
-
-static sgx_status_t SGX_CDECL enclave_exchange_report_ocall(void* pms)
-{
-	ms_exchange_report_ocall_t* ms = SGX_CAST(ms_exchange_report_ocall_t*, pms);
-	ms->ms_retval = exchange_report_ocall(ms->ms_sid, ms->ms_dh_msg2, ms->ms_dh_msg2_size, ms->ms_dh_msg3, ms->ms_dh_msg3_size, ms->ms_timeout);
-
-	return SGX_SUCCESS;
-}
-
-static sgx_status_t SGX_CDECL enclave_close_session_ocall(void* pms)
-{
-	ms_close_session_ocall_t* ms = SGX_CAST(ms_close_session_ocall_t*, pms);
-	ms->ms_retval = close_session_ocall(ms->ms_sid, ms->ms_timeout);
-
-	return SGX_SUCCESS;
-}
-
-static sgx_status_t SGX_CDECL enclave_invoke_service_ocall(void* pms)
-{
-	ms_invoke_service_ocall_t* ms = SGX_CAST(ms_invoke_service_ocall_t*, pms);
-	ms->ms_retval = invoke_service_ocall(ms->ms_pse_message_req, ms->ms_pse_message_req_size, ms->ms_pse_message_resp, ms->ms_pse_message_resp_size, ms->ms_timeout);
-
-	return SGX_SUCCESS;
-}
-
 static const struct {
 	size_t nr_ocall;
-	void * table[23];
+	void * table[19];
 } ocall_table_enclave = {
-	23,
+	19,
 	{
 		(void*)enclave_ocall_print,
 		(void*)enclave_ocall_enclave_print,
@@ -601,10 +536,6 @@ static const struct {
 		(void*)enclave_sgx_thread_set_untrusted_event_ocall,
 		(void*)enclave_sgx_thread_setwait_untrusted_events_ocall,
 		(void*)enclave_sgx_thread_set_multiple_untrusted_events_ocall,
-		(void*)enclave_create_session_ocall,
-		(void*)enclave_exchange_report_ocall,
-		(void*)enclave_close_session_ocall,
-		(void*)enclave_invoke_service_ocall,
 	}
 };
 sgx_status_t enclave_enclave_init_ra(sgx_enclave_id_t eid, sgx_status_t* retval, int b_pse, sgx_ra_context_t* p_context)
--- Samples/PSecureV1/Src/app/enclave_u.h
+++ Samples/PSecureV1/Src/app/enclave_u.h
@@ -8,6 +8,7 @@
 #include "sgx_edger8r.h" /* for sgx_status_t etc. */
 
 #include "sgx_key_exchange.h"
+#include "sgx_trts.h"
 
 #include <stdlib.h> /* for size_t */
 
@@ -92,22 +93,6 @@ int SGX_UBRIDGE(SGX_CDECL, sgx_thread_setwait_untrusted_events_ocall, (const voi
 #ifndef SGX_THREAD_SET_MULTIPLE_UNTRUSTED_EVENTS_OCALL_DEFINED__
 #define SGX_THREAD_SET_MULTIPLE_UNTRUSTED_EVENTS_OCALL_DEFINED__
 int SGX_UBRIDGE(SGX_CDECL, sgx_thread_set_multiple_untrusted_events_ocall, (const void** waiters, size_t total));
-#endif
-#ifndef CREATE_SESSION_OCALL_DEFINED__
-#define CREATE_SESSION_OCALL_DEFINED__
-sgx_status_t SGX_UBRIDGE(SGX_NOCONVENTION, create_session_ocall, (uint32_t* sid, uint8_t* dh_msg1, uint32_t dh_msg1_size, uint32_t timeout));
-#endif
-#ifndef EXCHANGE_REPORT_OCALL_DEFINED__
-#define EXCHANGE_REPORT_OCALL_DEFINED__
-sgx_status_t SGX_UBRIDGE(SGX_NOCONVENTION, exchange_report_ocall, (uint32_t sid, uint8_t* dh_msg2, uint32_t dh_msg2_size, uint8_t* dh_msg3, uint32_t dh_msg3_size, uint32_t timeout));
-#endif
-#ifndef CLOSE_SESSION_OCALL_DEFINED__
-#define CLOSE_SESSION_OCALL_DEFINED__
-sgx_status_t SGX_UBRIDGE(SGX_NOCONVENTION, close_session_ocall, (uint32_t sid, uint32_t timeout));
-#endif
-#ifndef INVOKE_SERVICE_OCALL_DEFINED__
-#define INVOKE_SERVICE_OCALL_DEFINED__
-sgx_status_t SGX_UBRIDGE(SGX_NOCONVENTION, invoke_service_ocall, (uint8_t* pse_message_req, uint32_t pse_message_req_size, uint8_t* pse_message_resp, uint32_t pse_message_resp_size, uint32_t timeout));
 #endif
 
 sgx_status_t enclave_enclave_init_ra(sgx_enclave_id_t eid, sgx_status_t* retval, int b_pse, sgx_ra_context_t* p_context);
